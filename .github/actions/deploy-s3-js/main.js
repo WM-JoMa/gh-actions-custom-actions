b2a6ee6d3fa5cb9f8e4d1231beb4e2c79de709c9
WM-JoMaDotio <msanchez@westmonroepartners.com>
2026-02-25 14:21:05 -0600

    adding module sintax

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 // const exect = require('@actions/exec')
 
 function run(){
