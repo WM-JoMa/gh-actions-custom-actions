004c2b3ec8d682777bd223af92ca5be3bfc4a9b8
WM-JoMaDotio <msanchez@westmonroepartners.com>
2026-02-25 13:54:53 -0600

    added custom js worker

---
+++ .github/actions/deploy-s3-js/main.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exect = require('@actions/exec')
+
+function run(){
+  core.notice("Hello form the js custom job")
+}
+
+run();
